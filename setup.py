b07cb571f92dea6ed3cee96dab7a43b15f8a0280
Naveen Kumar Battula <naveenkb1999@gmail.com>
2023-04-19 01:15:05 +0530

    test python package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='betp-libv',
-    version='0.1.0',
+    version='0.1.1',
     description='A sample Python package',
     author='Puddl',
     author_email='devppong@gmail.com',
