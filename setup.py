49f3a4b9e8483669146e768af417b012a171443d
Naveen Kumar Battula <naveenkb1999@gmail.com>
2023-04-19 01:34:49 +0530

    Fix the path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='betp-libv',
-    version='0.1.1',
+    version='0.1.2',
     description='A sample Python package',
     author='Puddl',
     author_email='devppong@gmail.com',
